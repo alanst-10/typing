37702b66bc56b4a4a366cf809a6985846bca6cf9
Alan de Santana Thimotheo <alanthimotheo@hotmail.com>
2021-09-20 11:49:36 -0300

    Projeto desenvolvido junto ao Professor Emerson Broga na Maratona Javascript 3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,100 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState, useRef } from "react";
+import wordList from './resources/words.json';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const MAX_TYPED_KEYS = 30;
+const WORD_ANIMATION_INTERVAL = 200;
+
+const getWord = () => {
+    const index = Math.floor(Math.random() * wordList.length);
+    const word = wordList[index];
+    return word.toLowerCase()
+}
+
+const isValidKey = (key, word) => {
+    if(!word) return false;
+    const result = word.split('').includes(key);
+    return result;
+}
+
+const Word = ({word, validKeys}) => {
+    if(!word) return null;
+    const joinedKeys = validKeys.join('');
+    const matched = word.slice(0, joinedKeys.length)
+    const remainder = word.slice(joinedKeys.length)
+
+    const matchedClass = (joinedKeys === word) ? 'matched completed' : 'matched';
+
+    return(<>
+        <span className={matchedClass}>{matched}</span>
+        <span className="remainder">{remainder}</span>
+    </>)
+}
+
+const App = () => {
+    const [ typedKeys, setTypedKeys ] = useState([]);
+    const [ validKeys, setValidKeys ] = useState([]);
+    const [ completedWords, setCompletedWords ] = useState([]);
+    const [ word, setWord ] = useState('');
+    const containerRef = useRef(null);
+
+    useEffect(() => {
+        setWord(getWord());
+        if(containerRef) containerRef.current.focus();
+    }, [] );
+
+    useEffect(() => {
+        const wordFromValidKeys = validKeys.join('').toLowerCase();
+
+        let timeout = null;
+        if(word && word === wordFromValidKeys) {
+            timeout = setTimeout(()=> {
+                // adicionar word ao completedWords
+                // limpar o array validKeys
+                // buscar uma nova word
+                let newWord = null;
+                do {
+                    newWord = getWord();
+                } while(completedWords.includes(newWord))
+
+                setWord(newWord);
+                setValidKeys([]);
+                setCompletedWords((prev) => [...prev, word]);
+            }, WORD_ANIMATION_INTERVAL);
+        }
+
+        return() => {
+            if(timeout) clearTimeout(timeout);
+        }
+
+    }, [word, validKeys, completedWords] );
+
+    const handleKeyDown = (e) => {
+        e.preventDefault();
+        const { key } = e;
+
+        setTypedKeys((prev) =>  [ ...prev, key ].slice(MAX_TYPED_KEYS * -1))
+
+        if(isValidKey(key, word)) {
+            setValidKeys((prev) => {
+                const isValidLength = prev.length <= word.length;
+                const isNextChar = isValidLength && word[prev.length] === key;
+                return (isNextChar) ? [...prev, key] : prev;
+            })
+        }
+    }
+    return (
+        <div className="container" tabIndex="0" onKeyDown={handleKeyDown} ref={containerRef}>
+            <div className="valid-keys">
+                <Word word={word} validKeys={validKeys} />
+            </div>
+            <div className="typed-keys">{ typedKeys ? typedKeys.join(' ') : null }</div>
+            <div className="completed-words">
+                <ol>
+                    { completedWords.map((word) => (<li key={word}>{word}</li>)) }
+                </ol>
+            </div>
+        </div>
+    );
 }
 
 export default App;
